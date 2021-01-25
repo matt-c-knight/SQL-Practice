c7132853a02e111f9befd7da08ce743645ddedf3
Matthew Knight <mattknight.sa@gmail.com>
2021-01-25 09:36:13 -0600

    setting up files

---
+++ codeup_bonus/worlddb.sql
@@ -0,0 +1,12 @@
+-- What languages are spoken in Santa Monica?
+
+-- How many different countries are in each region?
+
+-- What is the population for each region?
+
+-- What is the population for each continent?
+
+-- What is the average life expectancy globally?
+
+-- What is the average life expectancy for each region, each continent? Sort the results from shortest to longest
+
