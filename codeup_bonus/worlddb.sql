c113f6acabd211f6af77ad16ee53c4301bc0eae5
Matthew Knight <mattknight.sa@gmail.com>
2021-01-25 12:51:46 -0600

    world db queries

--- codeup_bonus/worlddb.sql
+++ codeup_bonus/worlddb.sql
@@ -1,6 +1,18 @@
 -- What languages are spoken in Santa Monica?
 
+
+SELECT Language, Percentage 
+from countrylanguage
+join city using(CountryCode)
+where city.Name = 'Santa Monica'
+order by Percentage desc;
+
 -- How many different countries are in each region?
+
+SELECT Region, count(Region) as num_countries
+from country
+Group By Region
+Order by num_countries asc;
 
 -- What is the population for each region?
 
