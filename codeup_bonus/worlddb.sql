7ac78b60a77b2f08d5e0d59beb284b2123766c13
Matthew Knight <mattknight.sa@gmail.com>
2021-01-26 10:26:23 -0600

    finished worlddb qs

--- codeup_bonus/worlddb.sql
+++ codeup_bonus/worlddb.sql
@@ -16,9 +16,32 @@ Order by num_countries asc;
 
 -- What is the population for each region?
 
+
+SELECT region, sum(population) as population
+from country
+group by region
+order by population desc;
+
 -- What is the population for each continent?
+
+SELECT continent, sum(population) as population
+from country
+group by continent
+order by population desc;
 
 -- What is the average life expectancy globally?
 
+SELECT avg(LifeExpectancy)
+from country;
+
 -- What is the average life expectancy for each region, each continent? Sort the results from shortest to longest
 
+SELECT Continent, avg(LifeExpectancy) as life_expectancy
+from country
+GROUP BY Continent
+ORDER BY life_expectancy asc;
+
+SELECT Region, avg(LifeExpectancy) as life_expectancy
+from country
+GROUP BY Region
+ORDER BY life_expectancy asc;
