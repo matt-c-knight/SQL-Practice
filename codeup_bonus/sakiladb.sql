250636acfe1dd620955fd663ff2568f42f9c8d5a
Matthew Knight <mattknight.sa@gmail.com>
2021-02-05 13:47:47 -0600

    continued practice w/ codeup dbs

---
+++ codeup_bonus/sakiladb.sql
@@ -0,0 +1,13 @@
+-- Display the first and last names in all lowercase of all the actors.
+
+
+-- You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you could use to obtain this information?
+
+
+-- Find all actors whose last name contain the letters "gen":
+
+
+-- Find all actors whose last names contain the letters "li". This time, order the rows by last name and first name, in that order.
+
+
+-- Using IN, display the country_id and country columns for the following countries: Afghanistan, Bangladesh, and China:
